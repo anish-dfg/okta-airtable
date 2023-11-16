519f8d3e3dcce24504619af7bccf2435619d3e8d
Anish Sinha <anish@developforgood.org>
2023-11-15 19:42:31 -0500

    Add License

--- main.py
+++ main.py
@@ -2,4 +2,6 @@ from pyairtable import Api
 from okta.client import Client as OktaClient
 
 if __name__ == "__main__":
+    api = Api("api-key-here")
+    table = api.table("", "")
     print("here")
