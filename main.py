270b6423fcf0448c8780e5df6d18bad9bedadf3f
Anish Sinha <anish@developforgood.org>
2023-11-15 23:36:21 -0500

    Add simple test file

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from pyairtable import Api
 from okta.client import Client as OktaClient
 
+
+# NOTE: This is an example function that is being tested in test_main.py. Feel free to remove it once some real code is added  # noqa: E501
+def add_ints(x: int, y: int) -> int:
+    return x + y
+
+
 if __name__ == "__main__":
-    api = Api("api-key-here")
-    table = api.table("", "")
-    print("here")
+    pass
---
+++ test_main.py
@@ -0,0 +1,5 @@
+from main import add_ints
+
+
+def test_add_ints():
+    assert add_ints(5, 5) == 10
