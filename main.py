6114c6d074e6a5b3a515ddd758d23e36e64418f8
Anish Sinha <anish@developforgood.org>
2023-11-17 16:49:09 -0500

    Add dotenv package and ensure airtable API works

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from pyairtable import Api
+import os
+
+from dotenv import load_dotenv
 from okta.client import Client as OktaClient
+from pyairtable import Api
 
 
 # NOTE: This is an example function that is being tested in test_main.py. Feel free to remove it once some real code is added  # noqa: E501
@@ -8,4 +11,6 @@ def add_ints(x: int, y: int) -> int:
 
 
 if __name__ == "__main__":
-    pass
+    load_dotenv()
+    api = Api(os.environ["AIRTABLE_ACCESS_TOKEN"])
+    table = api.table(os.environ["AIRTABLE_BASE_ID"], os.environ["AIRTABLE_TABLE_ID"])
